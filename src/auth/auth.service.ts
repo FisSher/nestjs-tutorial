e8250509b4407b20148d02bdd1c3a736322f01c9
Facundo <facundo.paolini@spritecloud.com>
2023-08-29 22:49:47 +0200

    added a database, started with auth service

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,8 +1,12 @@
 import { Injectable } from '@nestjs/common';
+import { PrismaService } from 'src/prisma/prisma.service';
+import { AuthDto } from './dto';
 
 @Injectable({})
 export class AuthService {
-  signup() {
+  constructor(private prisma: PrismaService) {}
+  signup(dto: AuthDto) {
+    console.log(dto);
     return { msg: 'Hola' };
   }
 
